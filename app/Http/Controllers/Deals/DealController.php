98c1ba9193a6166f307d3a373139ae7a5e2cfaa1
kenjua <33877693+kenjua@users.noreply.github.com>
2021-12-29 15:39:43 +0100

    New DealController.php

--- app/Http/Controllers/Deals/DealController.php
+++ app/Http/Controllers/Deals/DealController.php
@@ -6,37 +6,43 @@ class DealController extends Controller
 {
     public function checkExistActiveDealPrice(Request $request)
     {
-        $prices = Deal::find($request->post('id_deal'))->prices()->get();
-        $newStartDate = Carbon::parse($request->post('start_date'))->format('Y-m-d');
+		//recibimos dos variables id_deal y start_date y comprobamos si se reciben correctamente.
+		if(is_null($request->post('id_deal')) || is_null($request->post('start_date'))){
+			//Error. Devolveremos code = -1. He escogido -1 para diferenciar este error del del resto de code.
+			 return collect([ "code" => - 1])->toJson();
+			 exit;
+		}else{
+			$id = $request->post('id_deal');
+			$s_date = $request->post('start_date');
+		}
+		
+        $prices = Deal::find($id)->prices()->get();//Recibimos el precio
+        $newStartDate = Carbon::parse($s_date)->format('Y-m-d');//Recibimos la fecha
 
-        if( $prices->count() == 0 ) {
+		if($prices->count() < 1 ) {// comprobamos si hay resultados. 
             return collect([ "code" => 1 ])->toJson();
-        }
-
-        if( $prices->count() == 1 ) {
+        }else { //Si existe precio, mandamos precio y fecha al metodo para comprobar si la fecha es <, > o = al guardado en la tabla
             return $this->checkIfTheDateIslowerOrLaterOrEqualToTheRecordInTheTable($newStartDate, $prices->first())->toJson();
         }
-
-        $newDateBetweenSomePrice = Price::whereRaw('"'.Carbon::parse($request->post('start_date'))->format('Y-m-d').'" BETWEEN arm_deals_prices.start_date AND arm_deals_prices.end_date')->where('id_deal', $request->post('id_deal'));
-
-        if( $newDateBetweenSomePrice->count() == 0 ) {
-            $activePrices = Deal::find($request->post('id_deal'))->prices->where('end_date', '0000-00-00');
+		
+		//Buscamos el Price segun start_date en la tabla arm_deals_prices para un e id_deal.
+        $newDateBetweenSomePrice = Price::whereRaw('"'.Carbon::parse($s_date)->format('Y-m-d').'" BETWEEN arm_deals_prices.start_date AND arm_deals_prices.end_date')->where('id_deal', $id);
+		
+		//Si no hay resultados para Price
+        if( $newDateBetweenSomePrice->count() < 1 ) {
+            $activePrices = Deal::find($id)->prices->where('end_date', '0000-00-00');
             return $this->checkIfTheDateIslowerOrLaterOrEqualToTheRecordInTheTable($newStartDate, $activePrices->first())->toJson();
-        }
-
-        if( $newDateBetweenSomePrice->count() == 1 ) {
-            $nextPriceToNew = Price::where('id_deal', $request->post('id_deal'))->where('start_date', '>=', $newStartDate)->orderBy('start_date', 'ASC')->get()->first();
+        }else{//Si hay resultados para Price
+            $nextPriceToNew = Price::where('id_deal', $id)->where('start_date', '>=', $newStartDate)->orderBy('start_date', 'ASC')->get()->first();
             $newDateBetweenSomePrice = $newDateBetweenSomePrice->first();
 
             if ($newStartDate == $newDateBetweenSomePrice->start_date) {
-                return collect([
-                    "code" => 0
-                ])->toJson();
+                return collect(["code" => 0 ])->toJson();
             }
 
             return collect([
                     "code" => 4,
-                    "old_end_date" => Carbon::parse($request->post('start_date'))->subHours(24)->format('d-m-Y'),
+                    "old_end_date" => Carbon::parse($s_date)->subHours(24)->format('d-m-Y'),
                     "end_date" => Carbon::parse($nextPriceToNew->start_date)->subHours(24)->format('d-m-Y'),
                     "id_update_to_price_x" => $newDateBetweenSomePrice->id_deal_price,
                     "id_update_to_price_y" => $nextPriceToNew->id_deal_price
@@ -62,9 +68,7 @@ class DealController extends Controller
                     "id_update_to_price_x" => $price->id_deal_price
                 ]);
             } else {
-                return collect([
-                    "code" => 0
-                ]);
+                return collect(["code" => 0 ]);
             }
         } else{
             return collect([ "code" => 1 ]);
